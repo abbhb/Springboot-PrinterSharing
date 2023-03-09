5ad90d0bb66f477958810d022e236b818398563c
leo <3181538941@qq.com>
2023-03-09 18:59:16 +0800

    格式化一下代码, 将打印前参数校验部分重复代码整理为一个方法

--- src/main/java/com/qc/printers/service/impl/PrintServiceImpl.java
+++ src/main/java/com/qc/printers/service/impl/PrintServiceImpl.java
@@ -14,6 +14,8 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.io.FileInputStream;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.UUID;
 
 import static com.qc.printers.common.MyString.public_file;
@@ -21,39 +23,28 @@ import static com.qc.printers.common.MyString.public_file;
 @Service
 @Slf4j
 public class PrintServiceImpl implements PrintService {
-
+    
     private final PrinterService printerService;
-
+    
     @Autowired
     public PrintServiceImpl(PrinterService printerService) {
         this.printerService = printerService;
     }
-
+    
     @Transactional
     @Override
-    public boolean printsForPDF(String newName, String oldName, Integer numberOfPrintedPages,Integer printingDirection,Integer printBigValue,String numberOfPrintedPagesIndex,Long userId) {
-        if (numberOfPrintedPages==null){
-            throw new CustomException("err");
-        }
-        if (printingDirection==null){
-            throw new CustomException("err");
-        }
-        if (printBigValue==null){
-            throw new CustomException("err");
-        }
-        if (numberOfPrintedPagesIndex==null){
-            throw new CustomException("err");
-        }
+    public boolean printsForPDF(String newName, String oldName, Integer numberOfPrintedPages, Integer printingDirection, Integer printBigValue, String numberOfPrintedPagesIndex, Long userId) {
+        checkBeforePrint(numberOfPrintedPages, printingDirection, printBigValue, numberOfPrintedPagesIndex);
         try {
-            PdfReader pdfReader = new PdfReader(new FileInputStream(public_file+"\\"+newName));
-
+            PdfReader pdfReader = new PdfReader(Files.newInputStream(Paths.get(public_file + "\\" + newName)));
+            
             // 获取总页数
             int pages = pdfReader.getNumberOfPages();
 //            log.info("{}",pages);
             // 同文件多页 打印 就多次调用 printFile方法
             // i 为页码  当找不到时就停止打印了  ， 这里动态获取页码
-            if (numberOfPrintedPagesIndex.equals("all")){
-                PdfPrintUtil.printFile(public_file+"\\"+newName, "Brother HL-2240D series", newName,pages, numberOfPrintedPages, printingDirection, printBigValue);
+            if (numberOfPrintedPagesIndex.equals("all")) {
+                PdfPrintUtil.printFile(public_file + "\\" + newName, "Brother HL-2240D series", newName, pages, numberOfPrintedPages, printingDirection, printBigValue);
                 try {
                     Printer printer = new Printer();
                     printer.setName(newName);
@@ -63,14 +54,14 @@ public class PrintServiceImpl implements PrintService {
                     printer.setNumberOfPrintedPagesIndex(numberOfPrintedPagesIndex);
                     printer.setCreateUser(userId);
                     printerService.addPrinter(printer);
-                }catch (Exception e){
-                    //捕获异常，重在打印，记录没记上算了
-                    log.error("捕获异常:{}",e.getMessage());
+                } catch (Exception e) {
+                    // 捕获异常，重在打印，记录没记上算了
+                    log.error("捕获异常:{}", e.getMessage());
                 }
                 return true;
-            }else {
+            } else {
                 PdfPrintUtil.printFile(public_file + "\\" + newName, "Brother HL-2240D series", newName, Integer.parseInt(numberOfPrintedPagesIndex), numberOfPrintedPages, printingDirection, printBigValue);
-
+                
                 return true;
             }
         } catch (Exception e) {
@@ -78,34 +69,39 @@ public class PrintServiceImpl implements PrintService {
             e.printStackTrace();
             return false;
         }
-
+        
     }
-
+    
+    
     @Transactional
     @Override
-    public boolean printsForWord(String newName, String originName, Integer numberOfPrintedPages, Integer printingDirection, Integer printBigValue, String numberOfPrintedPagesIndex,Long userId) {
-        if (numberOfPrintedPages==null){
-            throw new CustomException("err");
-        }
-        if (printingDirection==null){
-            throw new CustomException("err");
-        }
-        if (printBigValue==null){
-            throw new CustomException("err");
-        }
-        if (numberOfPrintedPagesIndex==null){
-            throw new CustomException("err");
-        }
+    public boolean printsForWord(String newName, String originName, Integer numberOfPrintedPages, Integer printingDirection, Integer printBigValue, String numberOfPrintedPagesIndex, Long userId) {
+        checkBeforePrint(numberOfPrintedPages, printingDirection, printBigValue, numberOfPrintedPagesIndex);
         try {
-            String suffix = StringUtils.substringAfter(newName , ".");//后缀
+            String suffix = StringUtils.substringAfter(newName, ".");// 后缀
             String newNamePDF = UUID.randomUUID().toString() + ".pdf";
-            WordPrintUtil.wordToPDF(public_file+"\\"+newName,public_file+"\\"+newNamePDF);
-            //后面就调用pdf打印就行
-            return printsForPDF(newNamePDF,newNamePDF,numberOfPrintedPages,printingDirection,printBigValue,numberOfPrintedPagesIndex,userId);
+            WordPrintUtil.wordToPDF(public_file + "\\" + newName, public_file + "\\" + newNamePDF);
+            // 后面就调用pdf打印就行
+            return printsForPDF(newNamePDF, newNamePDF, numberOfPrintedPages, printingDirection, printBigValue, numberOfPrintedPagesIndex, userId);
         } catch (Exception e) {
             // TODO Auto-generated catch block
             e.printStackTrace();
             return false;
+        }
+    }
+    
+    private void checkBeforePrint(Integer numberOfPrintedPages, Integer printingDirection, Integer printBigValue, String numberOfPrintedPagesIndex) {
+        if (numberOfPrintedPages == null) {
+            throw new CustomException("err: numberOfPrintedPages is null");
+        }
+        if (printingDirection == null) {
+            throw new CustomException("err: printingDirection is null");
+        }
+        if (printBigValue == null) {
+            throw new CustomException("err: printBigValue is null");
+        }
+        if (numberOfPrintedPagesIndex == null) {
+            throw new CustomException("err: numberOfPrintedPagesIndex is null");
         }
     }
 }
